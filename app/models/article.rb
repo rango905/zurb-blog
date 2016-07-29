05717b8c8b03dfb5cf46870be5cb4be12c516fb6
rango905 <wuweiwei.chn@gmail.com>
2016-07-29 16:42:27 +0800

    fix join table delete bug
    when delete article or tag, will also delete article_keyword_tagship

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  has_many :article_keyword_tagships
+  has_many :article_keyword_tagships, dependent: :destroy
   has_many :keyword_tags, :through => :article_keyword_tagships
 
   validates :title, presence: true, length: { minimum: 3 }
--- app/models/keyword_tag.rb
+++ app/models/keyword_tag.rb
@@ -1,5 +1,5 @@
 class KeywordTag < ActiveRecord::Base
-  has_many :article_keyword_tagships
+  has_many :article_keyword_tagships, dependent: :destroy
   has_many :articles, :through => :article_keyword_tagships
   
   validates :name, length: { minimum: 2 }
